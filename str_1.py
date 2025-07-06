ff73ec1cdaa41a9e1e46019f2fc24d80b635f70e
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-07-06 13:48:57 +0530

    Add files via upload

---
+++ str_1.py
@@ -0,0 +1,52 @@
+##"""""" triple can do multiline string
+# a="ram"
+# print(a[0:2])
+
+s="pankaj"
+# print(s[-1])
+# print(s[5])
+
+# print(len(s))
+
+# for i in range(len(s)):
+# #     print(i,"--->",s[i])                    ------->positive indexing
+#      print(i-len(s),s[i-len(s)])     ----> negative indexing
+
+###########       slicing
+
+s="rhushi"
+# s[start index: end : step]
+
+# print(s[0:4])
+# print(s[:5])
+# print(s[:])
+
+# for i in s[::-1]:
+#     print(i)
+    
+    
+#     ######use of membership operator
+# s="rama is a god"
+# print('rama' in s)    
+
+
+# s=input("enter the string")
+# t=input("enter sub string you need to find")
+# if t in s:
+#     print("yes")
+# else:
+#     print('no')    
+
+
+
+
+######## palindrome
+# s=input("enter word to check its palindrome :")
+# if s[::-1]==s:                                          #--------> for palindrome have to use it
+#     print(s,"is a palindrome")
+# else:
+#     print(s,"is not a palindrome")   
+
+
+
+ 
