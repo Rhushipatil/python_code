6053c48b2da798e78485f9b0024970a166b5f930
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-07-15 00:25:04 +0530

    Add files via upload

---
+++ list/list_1.py
@@ -0,0 +1,61 @@
+###list
+
+##convert str to int 
+# s=input("enter a str:").split()
+# l=[]
+# for element in s:
+#     l.append(int(element))
+# print(l)
+
+
+
+####take a no and add that no of integers
+# a=int(input("enter the total no of "))
+# l=[]
+# for i in range(1,a):                 ------------> print the total no fo sequence in line
+#     l.append(int(i))
+# print(l)    
+    
+    
+# a=int(input("enter the total no of "))
+# b=input().split()
+# l=[]
+# for i in range(1,a):
+#     l.append(int(i))
+# print(l)        
+# print(type(l))
+
+## list comprehension
+# l=[ 0 for i in range(5)]  --->[0,0,0,0,0]
+# print(l)
+
+# l=[ 2*i for i in range(10)]
+# print(l)            ----> 2's table
+
+# l=[ i*i for i in range(10)]
+# print(l)
+
+###### insert method
+# l=[ i*i for i in range(10)]
+               #                         -----------> in list the id will remain same as it is mutable
+# l.insert(0,"ram")
+# print(l)
+
+#### extend keyword
+# a=[1,2,3]
+# b=[2,5,6]
+# a.extend(b)
+# print(a)
+# print(b)
+
+##pop
+# remove
+
+# sort
+# reverse
+
+
+# len
+# count
+# index 
+
