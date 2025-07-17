68404a6c3313e51c5ee5964b0b52df2efa298fa9
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-07-17 16:20:17 +0530

    Add files via upload

---
+++ set/set_1.py
@@ -0,0 +1,29 @@
+#### tuple
+# a=1,2
+# print(type(a))
+
+# a=(10+20) *30
+# print(a)
+# b=(10+20) *(30)          
+
+
+
+# a=(1,2,3)
+# d=a*3
+# print(d)
+
+#count
+# a=(1,2,3,4,5,6,8,7,9,22)
+# print(a.count(2))
+
+
+
+##set
+
+# a=set("rhushikesh")    ----> no duplicates and unordered
+# print(a)
+
+a=set([1,2,5,6,4,7])
+print(a)
+
+
