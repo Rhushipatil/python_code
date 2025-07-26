cae79d3deee983109bd2ce4e9005bee6ae4de95e
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-07-26 21:37:54 +0530

    Add files via upload

---
+++ dictionary/dict_2.py
@@ -0,0 +1,20 @@
+
+# s="rammmmmamamam"
+# print(s[::-1])
+
+# s="pankaj"
+# for i in range(0,s//2):
+#     temp=s[i]
+#     s[i]=s[]
+
+###pek element
+
+
+
+
+###union
+l = [1, 2, 3, 5, 6, 9]
+a = [2, 4, 6, 70, 90]
+
+result = set(l).union(set(a))
+print(result)
