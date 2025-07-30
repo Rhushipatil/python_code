0d1cfee181e592ae8e185f4410d9f0461fdd8258
Rhushikesh Patil <123874020+Rhushipatil@users.noreply.github.com>
2025-07-30 20:05:06 +0530

    Add files via upload

---
+++ functional prog/fp_3.py
@@ -0,0 +1,43 @@
+##lanbda function
+# s= lambda x:x*x*x
+
+# print(s(4))
+
+#inputs 2
+# a=lambda a,b :a*b
+# print(a(10,88))
+
+
+
+# 1]filter
+# 2]map
+# 3]reduce
+
+# def f(x):
+#     if x%3==0:
+#         return True
+#     else:
+#         return False
+    
+# w=list(filter(f,[10,20,30,40,50,60])  )    
+# print(w)  
+
+
+# w=list(filter(lambda x:x%3==0, [10,20,30,40,50,60]))  
+
+# print(w)
+
+
+##map
+# w=list(map(lambda x:x*x*x , [1,2,3,4,5]))
+# print(w)
+
+# a=[1,2,3,4]
+# b=[11,2,3,4,5]
+# w=list(map(lambda x,y:x+y , a,b))
+# print(w)
+
+# a=[1,2,3,4]
+
+# w=list(map(lambda x:x*x ,a))
+# print(w)
